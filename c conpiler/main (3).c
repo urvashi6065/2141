f703135231292657fe361b68b6dfc926f1c96ba7
RinkalRnw <117502014+RinkalRnw@users.noreply.github.com>
2023-07-06 18:19:59 +0530

    trio

---
+++ c conpiler/main (3).c
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");printf("Name    :urvashi\t");
+    
+    
+printf("dholakiya\t");
+printf("janakbhai\n"); 
+printf("course  :master in flutter\n"); 
+printf("M.no.   : 6359479453\n");
+printf("Age     : 17\n");
+
+printf("-  -  -  -  -  -  -\n");
+printf("|                  |\n");
+printf("u                  |\n");
+printf("r                  |\n");
+printf("v                  |\n");
+printf("a                  |\n");
+printf("s                  |\n");
+printf("h                  |\n");
+printf("i                  |\n");
+printf("-  -  -  -  -  -  -\t");
+
+
+
+
+printf("\n\n*");
+printf("\n*  *");
+printf("\n*  *   *");
+printf("\n*  *");
+printf("\n*");
+printf("\n\n*");
+printf("\n*");
+printf("\n*");
+printf("\n*");
+printf("\n*                 *  *                              *");
+printf("\n*           *           *                       *          *");
+printf("\n*       *                   *                *");
+printf("\n*   *                           *         *");
+printf("\n*                                   *  *");
+// printf("\n*");
+// printf("\n*");
+// printf("\n*");
+// printf("\n*\t*");
+// printf("\n*");
+
+
+
+
+
+return 0;
+}
+
+    
